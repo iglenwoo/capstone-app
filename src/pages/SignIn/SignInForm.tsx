16de9ba074340394dc1be3a8330710fca52b2706
Ingyu Woo <iglenwoo@gmail.com>
2019-10-15 21:58:14 -0700

    use material ui for signin page

--- src/pages/SignIn/SignInForm.tsx
+++ src/pages/SignIn/SignInForm.tsx
@@ -1,87 +1,156 @@
-import * as React from 'react'
+import React, { FC, useState } from 'react'
 import * as routes from '../../constants/routes'
 import { auth } from '../../firebase'
+import { Link } from 'react-router-dom'
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  CssBaseline,
+  FormControlLabel,
+  Grid,
+  Link as LinkUI,
+  makeStyles,
+  TextField,
+  Typography,
+} from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/core/SvgIcon/SvgIcon'
+import Copyright from '../../components/Copyright'
 
-interface InterfaceProps {
-  email?: string
-  error?: any
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}))
+
+export const SignInForm: FC<{
   history?: any
-  password?: string
-}
+}> = props => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [error, setError] = useState<string>()
 
-interface InterfaceState {
-  email: string
-  error: any
-  password: string
-}
+  const classes = useStyles()
 
-export class SignInForm extends React.Component<
-  InterfaceProps,
-  InterfaceState
-> {
-  private static INITIAL_STATE = {
-    email: '',
-    error: null,
-    password: '',
-  }
+  // private static propKey(propertyName: string, value: any): object {
+  //   return { [propertyName]: value }
+  // }
 
-  private static propKey(propertyName: string, value: any): object {
-    return { [propertyName]: value }
-  }
-
-  constructor(props: InterfaceProps) {
-    super(props)
-
-    this.state = { ...SignInForm.INITIAL_STATE }
-  }
-
-  public onSubmit = (event: any) => {
-    const { email, password } = this.state
-
-    const { history } = this.props
-
-    auth
-      .doSignInWithEmailAndPassword(email, password)
-      .then(() => {
-        this.setState(() => ({ ...SignInForm.INITIAL_STATE }))
-        history.push(routes.HOME)
-      })
-      .catch(error => {
-        this.setState(SignInForm.propKey('error', error))
-      })
+  const onSubmit = (event: any) => {
+    // const { email, password } = this.state
+    //
+    // const { history } = this.props
+    //
+    // auth
+    //   .doSignInWithEmailAndPassword(email, password)
+    //   .then(() => {
+    //     this.setState(() => ({ ...SignInForm.INITIAL_STATE }))
+    //     history.push(routes.HOME)
+    //   })
+    //   .catch(error => {
+    //     this.setState(SignInForm.propKey('error', error))
+    //   })
 
     event.preventDefault()
   }
 
-  public render() {
-    const { email, password, error } = this.state
-
-    const isInvalid = password === '' || email === ''
-
-    return (
-      <form onSubmit={event => this.onSubmit(event)}>
-        <input
-          value={email}
-          onChange={event => this.setStateWithEvent(event, 'email')}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          value={password}
-          onChange={event => this.setStateWithEvent(event, 'password')}
-          type="password"
-          placeholder="Password"
-        />
-        <button disabled={isInvalid} type="submit">
-          Sign In
-        </button>
-
-        {error && <p>{error.message}</p>}
-      </form>
-    )
-  }
-
-  private setStateWithEvent(event: any, columnType: string): void {
-    this.setState(SignInForm.propKey(columnType, (event.target as any).value))
-  }
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={password}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onSubmit={event => onSubmit(event)}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <LinkUI
+                href="#"
+                variant="body2"
+                component={Link}
+                to={routes.PASSWORD_FORGET}
+              >
+                Forgot password?
+              </LinkUI>
+            </Grid>
+            <Grid item>
+              <LinkUI
+                href="#"
+                variant="body2"
+                component={Link}
+                to={routes.SIGN_UP}
+              >
+                {"Don't have an account? Sign Up"}
+              </LinkUI>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  )
 }
