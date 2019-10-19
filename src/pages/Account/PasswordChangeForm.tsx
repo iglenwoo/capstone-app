026ae03bf4d4b6d568bd245fb12e64f30cb38a93
Ingyu Woo <iglenwoo@gmail.com>
2019-10-18 21:05:38 -0700

    WIP: convert to function component

--- src/pages/Account/PasswordChangeForm.tsx
+++ src/pages/Account/PasswordChangeForm.tsx
@@ -1,83 +1,36 @@
 import * as React from 'react'
+import { FC } from 'react'
+import { useState } from 'react'
 
-interface InterfaceProps {
-  error?: any
-  history?: any
-  passwordOne?: string
-  passwordTwo?: string
-}
+export const PasswordChangeForm: FC = () => {
+  const [passwordOne, setPasswordOne] = useState('')
+  const [passwordTwo, setPasswordTwo] = useState('')
+  const [isInvalid, setIsInvalid] = useState(true)
+  const [error, setError] = useState()
 
-interface InterfaceState {
-  error?: any
-  passwordOne?: string
-  passwordTwo?: string
-}
-
-export class PasswordChangeForm extends React.Component<
-  InterfaceProps,
-  InterfaceState
-> {
-  private static INITIAL_STATE = {
-    error: null,
-    passwordOne: '',
-    passwordTwo: '',
-  }
-
-  private static propKey(propertyName: string, value: string): object {
-    return { [propertyName]: value }
-  }
-
-  constructor(props: any) {
-    super(props)
-    this.state = { ...PasswordChangeForm.INITIAL_STATE }
-  }
-
-  public onSubmit = (event: any) => {
-    const { passwordOne }: any = this.state
-
-    // auth
-    //   .doPasswordUpdate(passwordOne)
-    //   .then(() => {
-    //     this.setState(() => ({ ...PasswordChangeForm.INITIAL_STATE }))
-    //   })
-    //   .catch(error => {
-    //     this.setState(PasswordChangeForm.propKey('error', error))
-    //   })
-
+  const onSubmit = (event: any) => {
     event.preventDefault()
   }
 
-  public render() {
-    const { passwordOne, passwordTwo, error }: any = this.state
+  return (
+    <form onSubmit={event => onSubmit(event)}>
+      <input
+        value={passwordOne}
+        onChange={event => setPasswordOne(event.currentTarget.value)}
+        type="password"
+        placeholder="New Password"
+      />
+      <input
+        value={passwordTwo}
+        onChange={event => setPasswordTwo(event.currentTarget.value)}
+        type="password"
+        placeholder="Confirm New Password"
+      />
+      <button disabled={isInvalid} type="submit">
+        Reset My Password
+      </button>
 
-    const isInvalid = passwordOne !== passwordTwo || passwordOne === ''
-
-    return (
-      <form onSubmit={event => this.onSubmit(event)}>
-        <input
-          value={passwordOne}
-          onChange={event => this.setStateWithEvent(event, 'passwordOne')}
-          type="password"
-          placeholder="New Password"
-        />
-        <input
-          value={passwordTwo}
-          onChange={event => this.setStateWithEvent(event, 'passwordTwo')}
-          type="password"
-          placeholder="Confirm New Password"
-        />
-        <button disabled={isInvalid} type="submit">
-          Reset My Password
-        </button>
-
-        {error && <p>{error.message}</p>}
-      </form>
-    )
-  }
-
-  private setStateWithEvent(event: any, columnType: string): void {
-    this.setState(
-      PasswordChangeForm.propKey(columnType, (event.target as any).value)
-    )
-  }
+      {error && <p>{error.message}</p>}
+    </form>
+  )
 }
