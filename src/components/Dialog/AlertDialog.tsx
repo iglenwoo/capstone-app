4d22361ed7927a21a2a96085f0b6856ee79d0754
Ingyu Woo <iglenwoo@gmail.com>
2019-11-07 20:52:41 -0800

    remove alert dialog title, change text color

--- src/components/Dialog/AlertDialog.tsx
+++ src/components/Dialog/AlertDialog.tsx
@@ -4,7 +4,6 @@ import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
-import DialogTitle from '@material-ui/core/DialogTitle'
 
 export const AlertDialog: FC<{
   message?: string
@@ -30,11 +29,8 @@ export const AlertDialog: FC<{
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
+          <DialogContentText id="alert-dialog-description" color="error">
             {props.message}
           </DialogContentText>
         </DialogContent>
