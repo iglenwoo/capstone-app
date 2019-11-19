517b2ddbff01efd939b91f7f9ef496b9d240ddbf
Ingyu Woo <iglenwoo@gmail.com>
2019-11-19 15:58:57 -0800

    remove Join component in project page

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react'
 import { Container } from '@material-ui/core'
 import { Create } from './Create'
-import { Join } from './Join'
 
 export const Projects = () => {
   return (
     <Container component="main" maxWidth="lg">
       <Create />
-      <Join />
     </Container>
   )
 }
