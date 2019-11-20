abdc7043d1ba29b3d63fe183546882dfa2bb5527
Ingyu Woo <iglenwoo@gmail.com>
2019-11-20 15:37:39 -0800

    display my projects

--- src/pages/MyProjects/index.tsx
+++ src/pages/MyProjects/index.tsx
@@ -1,13 +1,85 @@
 import * as React from 'react'
 import { Auth, useAuth } from '../../components/FirebaseAuth/use-auth'
+import {
+  Box,
+  Card,
+  CardContent,
+  CircularProgress,
+  Container,
+  createStyles,
+  Link as LinkUI,
+  makeStyles,
+  Theme,
+} from '@material-ui/core'
+import { Link } from 'react-router-dom'
+import * as routes from '../../constants/routes'
+import { useEffect, useState } from 'react'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    card: {
+      marginTop: theme.spacing(2),
+    },
+  })
+)
 
 export const MyProjects = () => {
-  const { user }: Auth = useAuth()
+  const { user, firestore }: Auth = useAuth()
+  const [projects, setProjects] = useState<string[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    if (user === null) return
+
+    firestore
+      .collection('users')
+      .doc(user.uid)
+      .get()
+      .then(doc => {
+        const data = doc.data()
+        if (data && data.projects) {
+          console.log(data.projects)
+          setProjects(data.projects)
+          setLoading(false)
+        }
+      })
+      .catch(error => {
+        console.log('Error getting document:', error)
+        setLoading(false)
+      })
+  }, [user, firestore])
+
+  const projectLinks = projects.map((p, i) => {
+    return (
+      <Box p={1}>
+        <LinkUI
+          href="#"
+          variant="body2"
+          component={Link}
+          to={`${routes.PROJECTS}/${p}`}
+          key={`${p}-${i}`}
+        >
+          {p}
+        </LinkUI>
+      </Box>
+    )
+  })
+
+  const classes = useStyles()
 
   return (
-    <div>
-      <h2>My Projects Page</h2>
-      {user && <p>user.email: {user.email}</p>}
-    </div>
+    <Container component="main" maxWidth="lg">
+      <Card className={classes.card}>
+        <CardContent>
+          {loading ? (
+            <Box display="flex" alignItems="center">
+              <CircularProgress color="secondary" />
+            </Box>
+          ) : (
+            <Box>{projectLinks}</Box>
+          )}
+        </CardContent>
+      </Card>
+    </Container>
   )
 }
