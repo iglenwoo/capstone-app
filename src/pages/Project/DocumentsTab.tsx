3064079e46fd10d4a3b98351700af8ccf0ac7a10
Ingyu Woo <iglenwoo@gmail.com>
2019-12-06 10:47:35 -0800

    add sub tasks for documents tab

--- src/pages/Project/DocumentsTab.tsx
+++ src/pages/Project/DocumentsTab.tsx
@@ -2,6 +2,17 @@ import { FC, default as React, useContext } from 'react'
 import { ProjectContext } from './index'
 
 // TODO: DocumentsTab
+/*
+  [ ] design /documents/ rules in firestore
+  [ ] add a document in firestore to test
+  [ ] fetch the doc
+  [ ] find a lib for text editor
+  [ ] npm add XXX
+  [ ] view(render) the doc
+  [ ] add new doc
+  [ ] edit a doc -> save
+  [ ] design side tabs and main doc body
+*/
 export const DocumentsTab: FC = () => {
   const { project } = useContext(ProjectContext)
 
