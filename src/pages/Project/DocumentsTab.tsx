bb2d23cba9085cee14059626d5fbc2c206c2bcba
Ingyu Woo <iglenwoo@gmail.com>
2019-12-06 22:17:12 -0800

    fetch documents

--- src/pages/Project/DocumentsTab.tsx
+++ src/pages/Project/DocumentsTab.tsx
@@ -1,10 +1,12 @@
 import { FC, default as React, useContext } from 'react'
 import { ProjectContext } from './index'
+import { useAuth } from '../../components/FirebaseAuth/use-auth'
+import { useAsyncEffect } from '../../utils/use-async-effect'
+import * as firebase from 'firebase'
+import { DOCUMENTS, PROJECTS } from '../../constants/routes'
 
 // TODO: DocumentsTab
 /*
-  [ ] design /documents/ rules in firestore
-  [ ] add a document in firestore to test
   [ ] fetch the doc
   [ ] find a lib for text editor
   [ ] npm add XXX
@@ -15,6 +17,27 @@ import { ProjectContext } from './index'
 */
 export const DocumentsTab: FC = () => {
   const { project } = useContext(ProjectContext)
+  const { firestore } = useAuth()
+
+  const fetchDocs = async () => {
+    if (!project.code) return
+
+    try {
+      const snapshot: firebase.firestore.QuerySnapshot = await firestore
+        .collection(PROJECTS)
+        .doc(project.code)
+        .collection(DOCUMENTS)
+        .get()
+
+      snapshot.forEach(result => {
+        console.log('result', result)
+      })
+    } catch (e) {
+      console.log('Error getting document:', e)
+    }
+  }
+
+  useAsyncEffect(fetchDocs, [project.code])
 
   return (
     <>
